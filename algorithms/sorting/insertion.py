48058e3c5e8cd26f9f8df74f048c136bd5b479ef
Rafael Salimov <raphasalimov@gmail.com>
2022-02-10 23:52:01 +0400

    added notes about insertion sort

--- algorithms/sorting/insertion.py
+++ algorithms/sorting/insertion.py
@@ -14,3 +14,9 @@ def insertion_sort(arr):
 data = [9, 5, 1, 4, 3]
 insertion_sort(data)
 print("Sorted", data)
+
+
+# An insertion sort compares values in turn, starting with the second value in the list.
+# If this value is greater than the value to the left of it, no changes are made. Otherwise this
+# value is repeatedly moved left until it meets a value that is less than it.
+# The sort process then starts again with the next value.
