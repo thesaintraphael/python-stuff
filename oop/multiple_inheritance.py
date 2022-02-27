26919f7008d47bd1619a1b47ed555484def69dd1
Rafael Salimov <raphasalimov@gmail.com>
2022-02-27 22:51:12 +0400

    multiple inheritance

---
+++ oop/multiple_inheritance.py
@@ -0,0 +1,48 @@
+class Rectangle:
+    def __init__(self, length, width, **kwargs):
+        self.length = length
+        self.width = width
+        super().__init__(**kwargs)
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * self.length + 2 * self.width
+
+
+class Square(Rectangle):
+    def __init__(self, length, **kwargs):
+        super().__init__(length=length, width=length, **kwargs)
+
+
+class Triangle:
+    def __init__(self, base, height, **kwargs):
+        self.base = base
+        self.height = height
+        super().__init__(**kwargs)
+
+    def tri_area(self):
+        return 0.5 * self.base * self.height
+
+
+class RightPyramid(Square, Triangle):
+    def __init__(self, base, slant_height, **kwargs):
+        self.base = base
+        self.slant_height = slant_height
+        kwargs["height"] = slant_height
+        kwargs["length"] = base
+        super().__init__(base=base, **kwargs)
+
+    def area(self):
+        base_area = super().area()
+        perimeter = super().perimeter()
+        return 0.5 * perimeter * self.slant_height + base_area
+
+    def area_2(self):
+        base_area = super().area()
+        triangle_area = super().tri_area()
+        return triangle_area * 4 + base_area
+
+
+p = RightPyramid(10, 5)
