7f78c82e41af0223c5f060d444b92accc1361fa8
Rafael Salimov <raphasalimov@gmail.com>
2022-05-04 19:37:09 +0400

    renamed file name

--- design_patterns/composite/example.py
+++ design_patterns/composite/example.py
@@ -1,53 +1,116 @@
-from abc import ABCMeta, abstractmethod
+"""
+Very advanced Employee management system.
+"""
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Optional
 
 
-class IDepartment(metaclass=ABCMeta):
+class Contract(ABC):
+    """Represents a contract anda payment process for a particular employeee."""
+
     @abstractmethod
-    def __init__(self, employees) -> None:
-        """implement in subclass"""
-
-    def print_department(self) -> None:
-        """implement in subclass"""
+    def get_payment(self) -> float:
+        """Compute how much to pay an employee under this contract."""
 
 
-class AccountingDepartment(IDepartment):
-    def __init__(self, employees) -> None:
-        self.employees = employees
+@dataclass
+class Commission(ABC):
+    """Represents a commission payment process."""
 
-    def print_department(self) -> None:
-        print(f"Accounting Department {self.employees}")
-
-
-class DevelopmentDepartment(IDepartment):
-    def __init__(self, employees) -> None:
-        self.employees = employees
-
-    def print_department(self) -> None:
-        print(f"Development Department {self.employees}")
+    @abstractmethod
+    def get_payment(self) -> float:
+        """Returns the commission to be paid out."""
 
 
-class ParentDepartment(IDepartment):
-    def __init__(self, employees) -> None:
-        self.employees = employees
-        self.base_employees = employees
-        self.sub_depts = []
+@dataclass
+class ContractCommission(Commission):
+    """Represents a commission payment process based on the number of contracts landed."""
 
-    def add(self, department):
-        self.sub_depts.append(department)
-        self.employees += department.employees
+    commission: float = 100
+    contracts_landed: int = 0
 
-    def print_department(self) -> None:
-        print("Parent Department")
-        print(f"Parent Department Base Employees: {self.base_employees}")
-        for department in self.sub_depts:
-            department.print_department()
+    def get_payment(self) -> float:
+        """Returns the commission to be paid out."""
+        return self.commission * self.contracts_landed
 
 
-dept1 = AccountingDepartment(200)
-dept2 = DevelopmentDepartment(200)
+@dataclass
+class Employee:
+    """Basic representation of an employee at the company."""
 
-parent_dept = ParentDepartment(30)
-parent_dept.add(dept1)
-parent_dept.add(dept2)
+    name: str
+    id_: int
+    contract: Contract
+    commission: Optional[Commission] = None
 
-parent_dept.print_department()
+    def compute_pay(self) -> float:
+        """Compute how much the employee should be paid."""
+        payout = self.contract.get_payment()
+        if self.commission is not None:
+            payout += self.commission.get_payment()
+        return payout
+
+
+@dataclass
+class HourlyContract(Contract):
+    """Contract type for an employee being paid on an hourly basis."""
+
+    pay_rate: float
+    hours_worked: int = 0
+    employer_cost: float = 1000
+
+    def get_payment(self) -> float:
+        return self.pay_rate * self.hours_worked + self.employer_cost
+
+
+@dataclass
+class SalariedContract(Contract):
+    """Contract type for an employee being paid a monthly salary."""
+
+    monthly_salary: float
+    percentage: float = 1
+
+    def get_payment(self) -> float:
+        return self.monthly_salary * self.percentage
+
+
+@dataclass
+class FreelancerContract(Contract):
+    """Contract type for a freelancer (paid on an hourly basis)."""
+
+    pay_rate: float
+    hours_worked: int = 0
+    vat_number: str = ""
+
+    def get_payment(self) -> float:
+        return self.pay_rate * self.hours_worked
+
+
+def main() -> None:
+    """Main function."""
+
+    henry_contract = HourlyContract(pay_rate=50, hours_worked=100)
+    henry = Employee(name="Henry", id_=12346, contract=henry_contract)
+    print(
+        f"{henry.name} worked for {henry_contract.hours_worked} hours "
+        f"and earned ${henry.compute_pay()}."
+    )
+
+    sarah_contract = SalariedContract(monthly_salary=5000)
+    sarah_commission = ContractCommission(contracts_landed=10)
+    sarah = Employee(
+        name="Sarah", id_=47832, contract=sarah_contract, commission=sarah_commission
+    )
+    print(
+        f"{sarah.name} landed {sarah_commission.contracts_landed} contracts "
+        f"and earned ${sarah.compute_pay()}."
+    )
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Source: https://github.com/ArjanCodes/2021-composition-vs-inheritance/blob/main/with_composition.py
