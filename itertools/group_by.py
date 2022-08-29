64c6f212f608a59a3f130fb211b9e1d9aaa351d6
Rafael Salimov <raphasalimov@gmail.com>
2022-08-29 22:03:43 +0400

    removed auto import

--- itertools/group_by.py
+++ itertools/group_by.py
@@ -1,21 +1,19 @@
 from itertools import groupby
-from tokenize import group
 
 people = [
     {"name": "Michael", "age": 19, "state": "NC"},
     {"name": "Andy", "age": 30, "state": "NC"},
     {"name": "Andy", "age": 26, "state": "NY"},
     {"name": "Justin", "age": 19, "state": "NC"},
-
 ]
 
 
 def get_state(person):
-    return person['state']
+    return person["state"]
 
 
 # list should be sorted with this key otherwise result will be unexpected (duplicates)
 person_state_group = groupby(sorted(people, key=get_state), key=get_state)
 
 for key, group in person_state_group:
-    print(f'{key}: {list(group)}')
+    print(f"{key}: {list(group)}")
