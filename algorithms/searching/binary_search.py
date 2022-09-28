303004b65edbcd26db34b7890d3aeb2346848abe
Rafael Salimov <raphasalimov@gmail.com>
2022-09-28 09:26:51 +0400

    testing location of found element to verify its first appearance

--- algorithms/searching/binary_search.py
+++ algorithms/searching/binary_search.py
@@ -1,20 +1,34 @@
+def test_location(numbers, target, mid):
+    mid_number = numbers[mid]
+
+    if mid_number == target:
+        return "left" if mid > 1 and numbers[mid - 1] == target else "found"
+    elif mid_number < target:
+        return "left"
+
+    else:
+        return "right"
+
+
 def binary_search(numbers, target):
 
     first, last = 0, len(numbers) - 1
-    index = -1
 
-    while first <= last and index == -1:
+    while first <= last:
         mid = (first + last) // 2
-        if numbers[mid] == target:
-            index = mid
-        elif target < numbers[mid]:
+        result = test_location(numbers, target, mid)
+
+        if result == "found":
+            return mid
+
+        elif result == "left":
             last = mid - 1
+
         else:
             first = mid + 1
 
-    return index
+    return -1
 
 
-numbers = [1, 2, 3, 4, 5]
-print(binary_search(numbers, 4))
+numbers = [8, 8, 6, 6, 6, 6, 6, 6, 3, 2, 2, 2, 0, 0, 0]
 print(binary_search(numbers, 6))
