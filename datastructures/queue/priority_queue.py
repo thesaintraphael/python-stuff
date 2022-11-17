a1e644111e0f3bca69dc213a653c00e450c27aaf
Rafael Salimov <raphasalimov@gmail.com>
2022-11-18 00:20:44 +0400

    Immplemented PriorityQueue with heap

--- datastructures/queue/priority_queue.py
+++ datastructures/queue/priority_queue.py
@@ -1,3 +1,28 @@
-# TODO: Implement a priority queue using a heap
+from heapq import heappush, heappop
+from itertools import count
 
-# Resource to use: https://realpython.com/queue-in-python/#implementing-queues-in-python
+
+class PriorityQueue:
+    def __init__(self) -> None:
+        self._elements = []
+        self._counter = count()
+
+    def enqueue_with_priority(self, priority, value):
+        heappush(self._elements, (-priority, next(self._counter), value))
+
+    def dequeue(self):
+        return heappop(self._elements)[-1]
+
+
+CRITICAL = 1
+IMPORTANT = 2
+NEUTRAL = 3
+
+messages = PriorityQueue()
+messages.enqueue_with_priority(CRITICAL, "Critical message")
+messages.enqueue_with_priority(IMPORTANT, "Important message")
+messages.enqueue_with_priority(NEUTRAL, "Neutral message")
+messages.enqueue_with_priority(IMPORTANT, "Another important message")
+
+print(messages.dequeue())
+print(messages.dequeue())
